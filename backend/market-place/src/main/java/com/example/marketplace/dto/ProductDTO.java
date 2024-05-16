1cbe93f608b9b107338087396477442412ddf578
Arturo Bendezu <artycok123@gmail.com>
2024-05-16 11:21:57 +0200

    Fixed Product structure and dto

--- backend/market-place/src/main/java/com/example/marketplace/dto/ProductDTO.java
+++ backend/market-place/src/main/java/com/example/marketplace/dto/ProductDTO.java
@@ -4,6 +4,6 @@ import com.example.marketplace.model.ProductState;
 import com.example.marketplace.model.Color;
 import com.example.marketplace.model.User;
 
-public record ProductDTO (String name, Double price, String yearOfProduction, Color color, String condition, ProductState state, String owner){
+public record ProductDTO (String name, Double price, String yearOfProduction, Color color, String condition, ProductState state, User seller, User buyer){
 }
 
